c0718866020f706d3a685cba69eec73c584c68c6
JWMassey <122041241+JWMassey@users.noreply.github.com>
2023-01-10 10:35:55 -0600

    Add files via upload

---
+++ JSBasicsExercise/script.js
@@ -0,0 +1,34 @@
+//First Exercise - Jack Massey
+
+console.log("Favorite Number: 7"); // Favorite Number: 7
+//Data Types: String, Number, Boolean, Null, Undefined (Symbol is the secret 6th)
+//Operator Symbols: +, -, *, /, **, %
+5 % 2;
+5 % 4;
+
+console.log('I\'M MARY POPPINS, Y\'ALL!'); //I'M MARY POPPINS, Y'ALL!
+console.log("Hawk" + "eye"); //Hawkeye
+
+console.log("Jack".length + "Massey".length) // 10
+
+console.log("Avengers Endgame"[15]); //e
+
+//Bonus
+
+console.log("\\"); // \
+
+console.log(5 ** 4 - 9); //616
+console.log(-"I".length + "Am".length - "Iron".length % "Man".length) //0
+
+console.log("CAP"[1]); //A
+console.log("VISION"[0]); //V
+console.log("OKOYE"[4]); //E
+console.log("NICK FURY"[0]); //N
+console.log("GROOT"[0]); //G
+console.log("DOCTOR STRANGE"[13]); //E
+console.log("THOR"[3]); //R
+console.log("STAR-LORD"[0]); //S
+
+m = 25
+c = 4
+u = m + c
